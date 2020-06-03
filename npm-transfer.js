30cb2ea4d12936ca14418ad9421a22c8a6387f82
Lotus <declininglotus@gmail.com>
2020-06-03 17:40:21 +0300

    Update npm-transfer.js

--- npm-transfer.js
+++ npm-transfer.js
@@ -2,7 +2,7 @@ const shell = require("shelljs")
 const { readdirSync } = require("fs")
 
 const transfer = package => {
-  shell.exec(`npm owner add npm lotusdevshack ${package}`) // Adds me as a maintainer on NPM alongside you.
+  shell.exec(`npm owner add lotusdevshack ${package}`) // Adds me as a maintainer on NPM alongside you.
   shell.exec(`npm access grant read-write Typefaces ${package}`) // Typefaces is the org name I assume.
 }
 
